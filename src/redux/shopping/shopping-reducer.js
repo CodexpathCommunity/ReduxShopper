5ac5d233df7b150e20d945328f8af3cf0be63694
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-15 20:22:57 +0100

    implemented a better switch statement

--- src/redux/shopping/shopping-reducer.js
+++ src/redux/shopping/shopping-reducer.js
@@ -50,22 +50,42 @@ const shopReducer = (state = INITIAL_STATE, action) => {
       const inCart = state.cart.find((item) =>
         item.id === action.payload.id ? true : false
       );
-      return {
-        ...state,
-        cart: inCart
-          ? state.cart.map((item) =>
-              item.id === action.payload.id
-                ? { ...item, qty: item.qty + 1 }
-                : item
-            )
-          : [
-              ...state.cart,
-              {
-                ...item,
-                qty: 1,
-              },
-            ],
-      };
+
+      if (inCart) {
+        return {
+          ...state,
+          cart: state.cart.map(
+            (x) => x.id === action.payload.id && { ...x, qty: x.qty + 1 }
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cart: [
+            ...state.cart,
+            {
+              ...item,
+              qty: 1,
+            },
+          ],
+        };
+      }
+    // return {
+    //   ...state,
+    //   cart: inCart
+    //     ? state.cart.map((item) =>
+    //         item.id === action.payload.id
+    //           ? { ...item, qty: item.qty + 1 }
+    //           : item
+    //       )
+    //     : [
+    //         ...state.cart,
+    //         {
+    //           ...item,
+    //           qty: 1,
+    //         },
+    //       ],
+    //};
     case actionTypes.REMOVE_FROM_CART:
       return {
         ...state,
